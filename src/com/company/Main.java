0b4e42744f9855ea46cbca6885e047a49cadc74d
arkan <arkan382382@outlook.com>
2018-10-04 21:10:35 +0200

    t2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,7 +6,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-
+//change with second branch
 public class Main {
 
     public static void main(String[] args) {
